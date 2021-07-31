51dc4831e27c16c04b2e6e1c0d193a1091c0ec38
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-07-31 20:31:36 +0530

    add async event listener

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-console.log(check)
+const form = document.querySelector('#searchForm')
+form.addEventListener('submit', async(e) => {
+    e.preventDefault()
+    // const searchTerm = form.elements.query.value
+    console.log(form.elements.query.value);
+})
