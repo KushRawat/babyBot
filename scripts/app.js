788b2cc892c9afb19d34d8ad00c1c6da5e09a48e
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-07-31 21:30:56 +0530

    catch error : code 401 unauthorized access

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,15 @@
-const form = document.querySelector('#searchForm')
-form.addEventListener('submit', async(e) => {
-    e.preventDefault()
-    // const searchTerm = form.elements.query.value
-    console.log(form.elements.query.value);
-})
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+  console.log(form.elements.query.value);
+  // MARK USER OPT-IN - https://www.gupshup.io/developer/docs/bot-platform/guide/whatsapp-api-documentation#optinAPI
+  await axios
+    .post("https://api.gupshup.io/sm/api/v1/app/opt/in/{{projectApp}}")
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+});
