eb58466862c7e98dd6b7e23c6db974ef97769f75
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-08-01 00:45:24 +0530

    error code 415 : unsupported format

--- scripts/app.js
+++ scripts/app.js
@@ -4,12 +4,26 @@ form.addEventListener("submit", async (e) => {
   const searchTerm = form.elements.query.value;
   console.log(form.elements.query.value);
   // MARK USER OPT-IN - https://www.gupshup.io/developer/docs/bot-platform/guide/whatsapp-api-documentation#optinAPI
-  await axios
-    .post("https://api.gupshup.io/sm/api/v1/app/opt/in/{{projectApp}}")
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-        console.log(err)
-    });
+  //   await axios
+  //     .post("https://api.gupshup.io/sm/api/v1/app/opt/in/{{projectApp}}")
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
 });
+
+const markOptIn = async () => {
+  const config = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      apikey: "nwkamkkbmfg7dhmuraehv0zbdjdeglnt",
+    },
+  };
+  const res = await axios.post(
+    "https://api.gupshup.io/sm/api/v1/app/opt/in/projectApp",
+    config
+  );
+  console.log(res);
+};
