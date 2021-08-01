0ba5b3e292c9ef4d7221c3bf6074dc3f213a4676
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-08-01 18:14:59 +0530

    opt-in

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,19 @@ const form = document.querySelector("#searchForm");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
-  console.log(form.elements.query.value);
+  console.log(searchTerm);
+
+  axios.defaults.headers.post["apikey"] = "nwkamkkbmfg7dhmuraehv0zbdjdeglnt";
+  const params = new URLSearchParams();
+  params.append("user", searchTerm);
+  axios.post("https://api.gupshup.io/sm/api/v1/app/opt/in/ProjectAppp", params);
+  // async function () {
+  //     axios({
+  //         method: 'post',
+  //         url:'https://api.gupshup.io/sm/api/v1/app/opt/in/projectApp'
+  //     })
+  // }
+  //   markOptIn();
   // MARK USER OPT-IN - https://www.gupshup.io/developer/docs/bot-platform/guide/whatsapp-api-documentation#optinAPI
   //   await axios
   //     .post("https://api.gupshup.io/sm/api/v1/app/opt/in/{{projectApp}}")
@@ -14,16 +26,16 @@ form.addEventListener("submit", async (e) => {
   //     });
 });
 
-const markOptIn = async () => {
-  const config = {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      apikey: "nwkamkkbmfg7dhmuraehv0zbdjdeglnt",
-    },
-  };
-  const res = await axios.post(
-    "https://api.gupshup.io/sm/api/v1/app/opt/in/projectApp",
-    config
-  );
-  console.log(res);
-};
+// const markOptIn = async () => {
+//   axios.defaults.headers.post["Content-Type"] =
+//     "application/x-www-form-urlencoded";
+//   const config = {
+//     headers: {
+//       apikey: "nwkamkkbmfg7dhmuraehv0zbdjdeglnt",
+//     },
+//   };
+//   const res = await axios.post(
+//     "https://api.gupshup.io/sm/api/v1/app/opt/in/projectApp",
+//     config
+//   );
+// };
