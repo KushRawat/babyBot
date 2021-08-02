13d9d9eb83b794b410ffe0e7ea1741306d96e1b0
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-08-02 18:44:24 +0530

    template mismatch

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,10 @@ form.addEventListener("submit", async (e) => {
   const searchTerm = form.elements.query.value;
   console.log(searchTerm);
 
+  // SETTING HEADERS FOR API CALLS
   axios.defaults.headers.post["apikey"] = "nwkamkkbmfg7dhmuraehv0zbdjdeglnt";
-  axios.defaults.headers.post["Content-Type"] =
-    "application/x-www-form-urlencoded";
+  // axios.defaults.headers.post["Content-Type"] =
+  //   "application/x-www-form-urlencoded";
 
   // MARK USER OPT IN
   let params1 = new URLSearchParams();
@@ -19,47 +20,16 @@ form.addEventListener("submit", async (e) => {
 
   // TEMPLATE MESSAGING
   let params2 = new URLSearchParams();
-  params2.append("channel", "whatsapp");
   params2.append("source", "917834811114");
   params2.append("destination", `91${searchTerm}`);
-  params2.append("src.name", "AppAccessAPI");
-  params2.append("message", {
-    id: "32ed47bd-ee1f-4202-a16a-42b10570e3f2",
-    params: ["word1", "word2"],
+  params2.append("template", {
+    id: "01a41e2a-4836-45b6-acb2-dc39587734b2",
+    params: ["Agent", "Local Address", "hello", "yolo"],
   });
-  let res2 = await axios.post("https://api.gupshup.io/sm/api/v1/msg", params2);
+  params2.append("src.name", "AppAccessAPI");
+  let res2 = await axios.post(
+    "http://api.gupshup.io/sm/api/v1/template/msg",
+    params2
+  );
   console.log(res2);
-
-  // MARK USER OPT IN
-
-  // async function () {
-  //     axios({
-  //         method: 'post',
-  //         url:'https://api.gupshup.io/sm/api/v1/app/opt/in/projectApp'
-  //     })
-  // }
-  //   markOptIn();
-  // MARK USER OPT-IN - https://www.gupshup.io/developer/docs/bot-platform/guide/whatsapp-api-documentation#optinAPI
-  //   await axios
-  //     .post("https://api.gupshup.io/sm/api/v1/app/opt/in/{{projectApp}}")
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
 });
-
-// const markOptIn = async () => {
-//   axios.defaults.headers.post["Content-Type"] =
-//     "application/x-www-form-urlencoded";
-//   const config = {
-//     headers: {
-//       apikey: "nwkamkkbmfg7dhmuraehv0zbdjdeglnt",
-//     },
-//   };
-//   const res = await axios.post(
-//     "https://api.gupshup.io/sm/api/v1/app/opt/in/projectApp",
-//     config
-//   );
-// };
